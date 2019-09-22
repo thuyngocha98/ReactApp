01ffda642dc681f901d4b3a538033c439dcc5e20
thuyngocha98 <41480357+thuyngocha98@users.noreply.github.com>
2019-09-22 10:05:57 +0700

    Revert "Revert "mainScreenGroup""
    This reverts commit bd433a784149965ed58043e2c4170e982a03acdf.

--- src/constants/Colors.tsx
+++ src/constants/Colors.tsx
@@ -2,7 +2,10 @@ const tintColor = 'rgba(247,189,66,1)';
 
 export default {
     tintColor,
+    mediumseagreen: '#3cb371',
     tabIconDefault: '#ccc',
+    gray: '#808080',
+    lightgray: '#D3D3D3',
     tabIconSelected: tintColor,
     tabBar: '#fefefe',
     errorBackground: 'red',
---
+++ src/styles/GroupsStyles/ListItemGroupStyles.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+import { screenWidth } from "../../constants/Dimensions";
+
+const ListItemGroupStyles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        marginHorizontal: 20,
+        marginVertical: 10,
+    },
+    container1: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    texts: {
+        flex: 1,
+        flexDirection: 'row',
+        margin: 5,
+    },
+    nameGroup: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        marginLeft: 10,
+        justifyContent: 'center'
+    },
+    textDetail: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: 'flex-end',
+        marginRight: 5
+    },
+    avatar: {
+        width: 40,
+        height: 40,
+        resizeMode: 'contain',
+    },
+    name: {
+        fontWeight: '400',
+        fontSize: 16
+    }
+});
+export default ListItemGroupStyles;
