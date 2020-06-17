e4f9c02349d03b7b2cd4e4279d760fe6e2bab44d
MinhTrung2031997 <46742785+MinhTrung2031997@users.noreply.github.com>
2020-06-17 19:38:36 +0700

    ip api

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -31,6 +31,7 @@ class SplashScreen extends Component<Props, States> {
     };
     receiveToken = async () => {
         try {
+            // AsyncStorage.removeItem('jwt').then(r => console.log(r));
             const value = await AsyncStorage.getItem('jwt');
             return value;
         } catch (error) {
