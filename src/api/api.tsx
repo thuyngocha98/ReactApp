ac0d17dcf334a311e335aa6d64ba849d6f829859
MinhTrung2031997 <46742785+MinhTrung2031997@users.noreply.github.com>
2020-06-26 17:48:29 +0700

    updating maps

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,4 +1,3 @@
-
 import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
@@ -10,112 +9,109 @@ export const BASEURL = 'http://192.168.20.150:3001';
 axios.defaults.withCredentials = true;
 
 export default {
+  // getProducts: async (pageIndex = 1, pageSize = 8) => {
+  //     try {
+  //         let url = BASEURL + 'api/be_tree/gettreesasync';
+  //         const response = await axios.get(url, { params: { pageIndex: pageIndex, pageSize: pageSize } });
+  //         // handle success
+  //         //console.log(response);
+  //         return response.data.data;
+  //     }
+  //     catch (error) {
+  //         // handle error
+  //         console.log(error);
+  //     }
+  // },
 
+  //
+  // postOrder: async (order) => {
+  //     try {
+  //         let url = BASEURL + '/api/BE_Order/CreateOrderAsync';
+  //         const response = await axios.post(url, {
+  //             "firstName": order.firstName,
+  //             "email": order.email,
+  //             "address": order.address,
+  //             "phone": order.phone,
+  //             "districtGuid": order.districtGuid,
+  //             "provinceGuid": order.provinceGuid,
+  //             "ghi_chu_cua_khach_hang": order.ghi_chu_cua_khach_hang,
+  //             "cartList": order.cartList
+  //         });
+  //         return response.data.data;
+  //     }
+  //     catch (error) {
+  //         console.log(error);
+  //     }
+  // },
 
-    // getProducts: async (pageIndex = 1, pageSize = 8) => {
-    //     try {
-    //         let url = BASEURL + 'api/be_tree/gettreesasync';
-    //         const response = await axios.get(url, { params: { pageIndex: pageIndex, pageSize: pageSize } });
-    //         // handle success
-    //         //console.log(response);
-    //         return response.data.data;
-    //     }
-    //     catch (error) {
-    //         // handle error
-    //         console.log(error);
-    //     }
-    // },
+  // get data user
+  _getDataUser: async () => {
+    const token = await AsyncStorage.getItem('jwt');
+    var dataUser = [];
+    const data = {
+      token: token,
+    };
+    const json = JSON.stringify(data);
+    await fetch(`${BASEURL}/api/user/get_info_user`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: json,
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        dataUser = res;
+      })
+      .catch((error) => {
+        alert(error);
+      });
+    return dataUser;
+  },
 
-    //
-    // postOrder: async (order) => {
-    //     try {
-    //         let url = BASEURL + '/api/BE_Order/CreateOrderAsync';
-    //         const response = await axios.post(url, {
-    //             "firstName": order.firstName,
-    //             "email": order.email,
-    //             "address": order.address,
-    //             "phone": order.phone,
-    //             "districtGuid": order.districtGuid,
-    //             "provinceGuid": order.provinceGuid,
-    //             "ghi_chu_cua_khach_hang": order.ghi_chu_cua_khach_hang,
-    //             "cartList": order.cartList
-    //         });
-    //         return response.data.data;
-    //     }
-    //     catch (error) {
-    //         console.log(error);
-    //     }
-    // },
+  // get list trip
+  getListAllTrip: async () => {
+    var listAllTrip = [];
+    await fetch(`${BASEURL}/api/trip/list_all_trip`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        listAllTrip = res.data;
+      })
+      .catch((error) => {
+        alert(error);
+      });
+    return listAllTrip;
+  },
 
+  // get list user by trip id
+  _getAllUserInTrip: async (tripId) => {
+    var listUserInTrip = [];
+    await fetch(`${BASEURL}/api/tripUser/list_all_user_by_tripId/${tripId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        listUserInTrip = res.data;
+      })
+      .catch((error) => {
+        alert(error);
+      });
+    return listUserInTrip;
+  },
 
-    // get data user
-    _getDataUser: async () => {
-        const token = await AsyncStorage.getItem('jwt');
-        var dataUser = [];
-        const data = {
-            token: token
-        };
-        const json = JSON.stringify(data);
-        await fetch(`${BASEURL}/api/user/get_info_user`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json'
-            },
-            body: json
-        })
-            .then((response) => response.json())
-            .then((res) => {
-                dataUser = res;
-            })
-            .catch((error) => {
-                alert(error);
-            });
-        return dataUser;
-    },
-
-    // get list trip
-    getListAllTrip: async () => {
-        var listAllTrip = [];
-        await fetch(`${BASEURL}/api/trip/list_all_trip`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json'
-            },
-        })
-            .then((response) => response.json())
-            .then((res) => {
-                listAllTrip = res.data;
-            })
-            .catch((error) => {
-                alert(error);
-            });
-        return listAllTrip;
-    },
-
-    // get list user by trip id
-    _getAllUserInTrip: async tripId => {
-        var listUserInTrip = [];
-        await fetch(`${BASEURL}/api/tripUser/list_all_user_by_tripId/${tripId}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json'
-            },
-        })
-            .then((response) => response.json())
-            .then((res) => {
-                listUserInTrip = res.data;
-            })
-            .catch((error) => {
-                alert(error);
-            });
-        return listUserInTrip;
-    },
-
-    // save trip id
-    _saveTripId: async tripId => {
-        return tripId;
-    }
-}
+  // save trip id
+  _saveTripId: async (tripId) => {
+    return tripId;
+  },
+};
