00389be94dc911921ebe159f1351384bfcefc2b1
hathuy <ha.thuy@namlongsoft.net>
2020-08-02 21:21:35 +0700

    commit package lock

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -4,45 +4,11 @@ import { AsyncStorage } from 'react-native';
 const TIMEOUT = 100;
 
 // 52.187.177.128
-export const BASEURL = 'http://192.168.1.11:3001';
+export const BASEURL = 'http://192.168.1.6:3001';
 
 axios.defaults.withCredentials = true;
 
 export default {
-  // getProducts: async (pageIndex = 1, pageSize = 8) => {
-  //     try {
-  //         let url = BASEURL + 'api/be_tree/gettreesasync';
-  //         const response = await axios.get(url, { params: { pageIndex: pageIndex, pageSize: pageSize } });
-  //         // handle success
-  //         //console.log(response);
-  //         return response.data.data;
-  //     }
-  //     catch (error) {
-  //         // handle error
-  //         console.log(error);
-  //     }
-  // },
-
-  //
-  // postOrder: async (order) => {
-  //     try {
-  //         let url = BASEURL + '/api/BE_Order/CreateOrderAsync';
-  //         const response = await axios.post(url, {
-  //             "firstName": order.firstName,
-  //             "email": order.email,
-  //             "address": order.address,
-  //             "phone": order.phone,
-  //             "districtGuid": order.districtGuid,
-  //             "provinceGuid": order.provinceGuid,
-  //             "ghi_chu_cua_khach_hang": order.ghi_chu_cua_khach_hang,
-  //             "cartList": order.cartList
-  //         });
-  //         return response.data.data;
-  //     }
-  //     catch (error) {
-  //         console.log(error);
-  //     }
-  // },
 
   // get data user
   _getDataUser: async () => {
